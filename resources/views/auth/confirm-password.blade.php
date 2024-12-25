cbbfd3db7dac59887b36fbcd2a427fc131d8976e
mas <m.yoshida553@gmail.com>
2024-12-26 02:15:11 +0900

    日本語化

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
+        {{ __('このアプリケーションの安全な領域です。パスワードを確認して続行してください。') }}
     </div>
 
     <form method="POST" action="{{ route('password.confirm') }}">
@@ -11,16 +11,16 @@
             <x-input-label for="password" :value="__('Password')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+                type="password"
+                name="password"
+                required autocomplete="current-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <div class="flex justify-end mt-4">
             <x-primary-button>
-                {{ __('Confirm') }}
+                {{ __('パスワードを確認') }}
             </x-primary-button>
         </div>
     </form>
