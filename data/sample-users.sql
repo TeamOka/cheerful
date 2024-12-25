4ab2269308c882fb23a3cfb7998e6975bcbadb84
mas <m.yoshida553@gmail.com>
2024-12-25 23:42:10 +0900

    ダミーデータの追加とデザイン修正

---
+++ data/sample-users.sql
@@ -0,0 +1,11 @@
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, '佐藤 太郎', 'sato.taro@example.com', NULL, '$2y$12$7COeWWA9UQ05XPK7ava6a.MWzncoJvV.DvmtgH6vkI50xYMiXF3uS', NULL, NOW(), NOW()),
+(2, '鈴木 次郎', 'suzuki.jiro@example.com', NULL, '$2y$12$7COeWWA9UQ05XPK7ava6a.MWzncoJvV.DvmtgH6vkI50xYMiXF3uS', NULL, NOW(), NOW()),
+(3, '高橋 三郎', 'takahashi.saburo@example.com', NULL, '$2y$12$7COeWWA9UQ05XPK7ava6a.MWzncoJvV.DvmtgH6vkI50xYMiXF3uS', NULL, NOW(), NOW()),
+(4, '田中 四郎', 'tanaka.shiro@example.com', NULL, '$2y$12$7COeWWA9UQ05XPK7ava6a.MWzncoJvV.DvmtgH6vkI50xYMiXF3uS', NULL, NOW(), NOW()),
+(5, '山本 五郎', 'yamamoto.goro@example.com', NULL, '$2y$12$7COeWWA9UQ05XPK7ava6a.MWzncoJvV.DvmtgH6vkI50xYMiXF3uS', NULL, NOW(), NOW()),
+(6, '中村 六郎', 'nakamura.roku@example.com', NULL, '$2y$12$7COeWWA9UQ05XPK7ava6a.MWzncoJvV.DvmtgH6vkI50xYMiXF3uS', NULL, NOW(), NOW()),
+(7, '小林 七郎', 'kobayashi.shichiro@example.com', NULL, '$2y$12$7COeWWA9UQ05XPK7ava6a.MWzncoJvV.DvmtgH6vkI50xYMiXF3uS', NULL, NOW(), NOW()),
+(8, '加藤 八郎', 'kato.hachiro@example.com', NULL, '$2y$12$7COeWWA9UQ05XPK7ava6a.MWzncoJvV.DvmtgH6vkI50xYMiXF3uS', NULL, NOW(), NOW()),
+(9, '吉田 九郎', 'yoshida.kuro@example.com', NULL, '$2y$12$7COeWWA9UQ05XPK7ava6a.MWzncoJvV.DvmtgH6vkI50xYMiXF3uS', NULL, NOW(), NOW()),
+(10, '山田 十郎', 'yamada.juro@example.com', NULL, '$2y$12$7COeWWA9UQ05XPK7ava6a.MWzncoJvV.DvmtgH6vkI50xYMiXF3uS', NULL, NOW(), NOW());
