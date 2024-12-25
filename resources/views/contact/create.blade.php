2ea900995a6422055e8f7376743050b320065cd3
0824mami <mami0824@gmail.com>
2024-12-26 05:05:26 +0900

    詳細画面にセレクトボックスを追加し、不要なProduct＿idを削除

--- resources/views/contact/create.blade.php
+++ resources/views/contact/create.blade.php
@@ -16,7 +16,7 @@
                         {{-- 名前 --}}
                         <div class="mb-4">
                             <label for="name" class="block text-gray-700 text-sm font-bold mb-2">
-                                お名前
+                                名前
                             </label>
                             <input type="text" name="name" id="name" 
                                 class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
